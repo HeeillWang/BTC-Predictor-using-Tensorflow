a6acf70ab99cf881c658f90471b37f80b1e61638
showx123 <showx123@naver.com>
2017-12-27 18:10:06 +0900

    RNN building...

--- RNNs/train.py
+++ RNNs/train.py
@@ -20,6 +20,16 @@ def MinMaxScaler(data):
     data *= 1 / (np.max(np.abs(data),axis=0) + 1e-8)
     return data
 
+'''
+parameter
+
+data : numpy array
+
+------------------
+returns 
+
+created dataset numpy array.
+'''
 def MakeDataSet(data, num_seq):
     ret = []
     
@@ -32,13 +42,18 @@ num_input = 5
 num_seq = 10
 num_output = 1
 num_hidden = 2
-batch_size = 100
 learning_rate = 0.01
 epoch = 100
+data_split_rate = 0.7	# dataset split rate for train data. Others will be test data
 
-x = np.loadtxt('data.csv', delimiter=',', skiprows=1)
-x = MinMaxScaler(data)
 
-y = xy[:,[-1]]	# close price will be label
+x = np.loadtxt('hourly_data.csv', delimiter=',', skiprows=1)
+x = MinMaxScaler(x)
+
+y = x[:,[-1]]	# close price will be label
+
+
+x = MakeDataSet(x, num_seq)	# shape = [None, num_seq, num_input]
+y = MakeDataSet(y, num_seq)	# shape = [None, num_seq, num_output]
 
 
