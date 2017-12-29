28964e591ab52004940cc8ff4ff293c870901c4b
showx123 <showx123@naver.com>
2017-12-29 15:19:27 +0900

    RNN one_hot prediction completed

--- RNNs/train_one_hot.py
+++ RNNs/train_one_hot.py
@@ -40,16 +40,19 @@ def MakeDataSet(data, num_seq, pos):
     for i in range(len(data) - num_seq):
         x.append(data[i:i+num_seq])
         temp = []
-        y.append([data[i+num_seq][pos]])
+        if(data[i+num_seq-1][pos] > data[i+num_seq][pos]):
+            y.append([0])   # up
+        else:
+            y.append([1])   # down
 
     return (x,y)
 
 num_input = 5
 num_seq = 10
-num_output = 1
+num_output = 2  # number of output class by one_hot
 num_hidden = 2
 learning_rate = 0.01
-epoch = 100
+epoch = 500
 data_split_rate = 0.7	# dataset split rate for train data. Others will be test data
 
 
@@ -70,19 +73,17 @@ test_x = x[train_len:]
 test_y = y[train_len:]
 
 X = tf.placeholder(tf.float32, [None, num_seq, num_input])
-Y= tf.placeholder(tf.float32, [None, num_output])
+Y= tf.placeholder(tf.int32, [None, 1])
+Y_one_hot = tf.one_hot(Y, num_output)  # one hot
+Y_one_hot = tf.reshape(Y_one_hot, [-1, num_output])
 
 #RNN Model + fully-connected
 cell = tf.contrib.rnn.BasicLSTMCell(num_units=num_hidden, state_is_tuple=True, activation=tf.tanh)
 outputs, _ = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
 predict = tf.contrib.layers.fully_connected(outputs[:,-1], num_output, activation_fn=None)  # use last cell's output
-cost = tf.reduce_sum(tf.square(predict - Y))    # MSE
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predict, labels=Y_one_hot))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
-# RMSE for accuracy test
-targets = tf.placeholder(tf.float32, [None, 1])
-predictions = tf.placeholder(tf.float32, [None, 1])
-rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
 
 
 with tf.Session() as sess:
@@ -92,9 +93,10 @@ with tf.Session() as sess:
     # Training
     for i in range(epoch):
         _, loss = sess.run([optimizer, cost], feed_dict = {X : train_x, Y:train_y})
-        print("i ", epoch, " : ", loss)
+        print("Epoch ", i, " : ", loss)
 
     # Testing
-    test_predict = sess.run(predict, feed_dict={X:test_x})
-    rmse_val = sess.run(rmse, feed_dict={targets:test_y, predictions:test_predict})
-    print("RMSE for test data : ", rmse_val)
+    correct_prediction = tf.equal(tf.argmax(predict, 1), tf.argmax(Y_one_hot, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('Accuracy:', sess.run(accuracy, feed_dict={X: test_x, Y: test_y}))
+
