2177d2e8d9f1721c6a1fcc2cf7b807d662061f12
showx123 <showx123@naver.com>
2018-01-07 14:45:33 +0900

    basic crawler is completed, need to be updated and refactoring later

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,14 +1,28 @@
 import requests as rq
 import time
+import numpy as np
 
+interval = 1638000000
+start = 1356966000000
+end = 1514732400000
 url = 'http://index.bithumb.com/api/coinmarketcap/localAPI.php'
-res = rq.get(url, params={
-    'api': 'graph',
-    'coin': 'btc',
-    'subject': 'price_usd',
-    'start': 1451617200000,
-    'end': 1452826800000
-})
 
-for i in range(len(res.json())):
-    print(time.ctime(res.json()[i][0] / 1000), res.json()[i][1])
+target = start
+arr = []
+
+while(target + interval <= 1514732400000):
+    res = rq.get(url, params={
+        'api': 'graph',
+        'coin': 'btc',
+        'subject': 'price_usd',
+        'start': target,
+        'end': target + interval
+    })
+
+    target = target + interval
+
+    for i in range(len(res.json())):
+        arr.append([time.ctime(res.json()[i][0] / 1000), res.json()[i][1]])
+
+
+np.savetxt("data.csv", arr,delimiter=',', fmt="%s")
