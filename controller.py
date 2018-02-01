666e0c7cd567d3104364fbe4f78ff7d14258bd77
showx123 <showx123@naver.com>
2018-02-01 20:45:26 +0900

    model trained

--- controller.py
+++ controller.py
@@ -2,9 +2,9 @@ import  sys
 sys.path.append('./Crawler')
 sys.path.append('./RNNs')
 
-import crawler
+#import crawler
 import train
 
 
-#train.train('./RNNs')
-crawler.collect_data('data.csv',['btc'])
+train.train('./RNNs')
+# crawler.collect_data('data.csv',['btc'])
